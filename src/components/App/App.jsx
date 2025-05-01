bbb19445eeaca6cce567e1ab3557d25e22e69760
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-05-01 21:03:28 +0200

    build: jsconfig, improve routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,29 @@
 import SharedLayout from "-/components/SharedLayout/SharedLayout";
 import { Home } from "-/components/Home/Home";
 import { Calendar } from "-/components/Calendar/Calendar";
-import { OrdersList } from "-/components/OrdersList/OrdersList";
-import { DriversList } from "-/components/DriversList/DriversList";
-import { Settings } from "-/components/Settings/Settings";
-import { OrderPage } from "-/components/OrderPage/OrderPage";
+import { OrdersList } from "components/OrdersList/OrdersList";
+import { DriversList } from "components/DriversList/DriversList";
+import { Settings } from "components/Settings/Settings";
+import { OrderPage } from "components/OrderPage/OrderPage";
 import { Route, Routes } from "react-router-dom";
+import { CreateOrderPage } from "components/CreateOrderPage/CreateOrderPage";
+import { Sender } from "components/CreateOrderPage/Sender";
+import { OrderInfo } from "components/CreateOrderPage/OrderInfo";
+import { Recipient } from "components/CreateOrderPage/Recipient";
+import { LogInPage } from "components/LogIn/LogInPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />}></Route>
+        <Route path="createorder" element={<CreateOrderPage />}>
+          <Route path="sender" element={<Sender />}></Route>
+          <Route path="orderinfo" element={<OrderInfo />}></Route>
+          <Route path="recipient" element={<Recipient />}></Route>
+        </Route>
+        <Route path="orders/:orderId" element={<OrderPage />}></Route>
+        <Route path="login" element={<LogInPage />}></Route>
         <Route path="admin">
           <Route path="calendar" element={<Calendar />}></Route>
           <Route path="orders" element={<OrdersList />}></Route>
@@ -19,9 +31,9 @@ const App = () => {
           <Route path="settings" element={<Settings />}></Route>
         </Route>
         <Route path="driver">
-          <Route path="/driver/orders" element={<OrdersList />}></Route>
+          <Route path="myorders" element={<OrdersList />}></Route>
+          <Route path="settings" element={<Settings />}></Route>
         </Route>
-        <Route path="orders/:orderId" element={<OrderPage />}></Route>
       </Route>
     </Routes>
   );
---
+++ src/components/CreateOrderPage/Recipient.jsx
@@ -0,0 +1,3 @@
+export const Recipient = () => {
+  return <div>Recipient</div>;
+};
