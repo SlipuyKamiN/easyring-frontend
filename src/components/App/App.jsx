c6f9612f918c0d9d34635da635f32b438d272838
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-05-27 22:38:28 +0200

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,8 @@ import { SignInPage } from "~/components/SignIn/SignInPage";
 import { Confirm } from "../CreateOrderPage/Confirm";
 
 const App = () => {
+  console.log(import.meta.env.VITE_BASE_URL);
+
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
