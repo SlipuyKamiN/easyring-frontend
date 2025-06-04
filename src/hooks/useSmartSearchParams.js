4826fb338a6df74d381fe320e67dc1ae61be6d95
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-06-04 11:24:29 +0200

    fix: filter/query/params logic

--- src/hooks/useSmartSearchParams.js
+++ src/hooks/useSmartSearchParams.js
@@ -1,16 +1,31 @@
 import { parseISO } from "date-fns";
-import { useSelector } from "react-redux";
+import { useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getISOTodayDate } from "~/helpers/getISOTodayDate";
-import { getUserState } from "~/Redux/userSelectors";
 
-export const useSmartSearchParams = () => {
-  const user = useSelector(getUserState);
-  const [searchParams, setSearchParams] = useSearchParams({
-    date: getISOTodayDate(),
-    driver: user._id,
-  });
+export const useSmartSearchParams = (user) => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const params = Object.fromEntries([...searchParams]);
+  const hasInitialized = useRef(false);
+
+  useEffect(() => {
+    if (!hasInitialized.current && user?._id) {
+      const isEmpty = searchParams.size === 0;
+
+      if (isEmpty) {
+        const today = getISOTodayDate();
+
+        const defaults = {
+          driver: user._id,
+          date: today,
+        };
+
+        setSearchParams(defaults, { replace: true });
+      }
+
+      hasInitialized.current = true;
+    }
+  }, [searchParams, setSearchParams, user]);
 
   const updateParam = (key, value) => {
     if (!value || value === "1970-01-01T01:00:00+01:00") {
--- src/pages/ParcelsPage.jsx
+++ src/pages/ParcelsPage.jsx
@@ -4,20 +4,30 @@ import { useSearchParcelsQuery } from "~/Redux/parcelsSlice";
 import { ParcelsList } from "~/components/ParcelsList/ParcelsList";
 import { useSelector } from "react-redux";
 import { getUserState } from "~/Redux/userSelectors";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ParcelsPage = () => {
   const user = useSelector(getUserState);
   const isAdmin = user.role === "admin";
-  const { searchParams, updateParam, get } = useSmartSearchParams();
-  const [query, setQuery] = useState(searchParams.toString());
-  const { data } = useSearchParcelsQuery(query);
+  const { searchParams, updateParam, get } = useSmartSearchParams(user);
+  const [query, setQuery] = useState("");
+  const { data, error } = useSearchParcelsQuery(query);
+
+  console.log(error);
+
+  useEffect(() => {
+    if (searchParams.size >= 1 && !query) {
+      setQuery(searchParams.toString());
+    }
+  }, [query, searchParams]);
 
   return (
     <>
       <Filter props={{ setQuery, searchParams, updateParam, get, isAdmin }} />
-      {!data && <div>There are no parcels for today!</div>}
-      {data && <ParcelsList parcels={data.parcels} isAdmin={isAdmin} />}
+      {!data || (error && <div>There are no parcels for today!</div>)}
+      {!error && data && (
+        <ParcelsList parcels={data.parcels} isAdmin={isAdmin} />
+      )}
     </>
   );
 };
