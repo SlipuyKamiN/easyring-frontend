add0cc4f825e7341ba6c3fc18752a76b0864ac07
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-06-05 21:08:09 +0200

    build: PWA config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["easyRing-arrow.svg"],
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": resolve(__dirname, "src"),
