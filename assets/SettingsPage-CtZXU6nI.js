a51ab541fad174d38dfd42106ad108dc359899bd
SlipuyKamiN <SlipuyKamiN@users.noreply.github.com>
2025-10-11 18:03:39 +0000

    deploy: c2996700ddebce115d9bf30bc090bee8a695eae1

--- assets/SettingsPage-CtZXU6nI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,C as e}from"./index-BEV_3721.js";const n=()=>t.jsx(e,{children:"SettingsPage"});export{n as default};
--- assets/UsersListPage-YqKhuQno.js
+++ assets/UsersListPage-YqKhuQno.js
@@ -1,4 +1,4 @@
-import{q as h,t as g,ad as C,ae as b,r as c,j as e,G as $,H as v,I as y,K as R,N as U,a as L,n as k,X as S,Y as E,S as N,C as D}from"./index-BEV_3721.js";import{R as w,C as A}from"./ParcelsList.styled-BKerZOhL.js";const F=h.ul`
+import{q as h,t as g,ad as C,ae as b,r as c,j as e,G as $,H as v,I as y,K as R,N as U,a as L,n as k,X as S,Y as E,S as N,C as D}from"./index-C1uBPyO1.js";import{R as w,C as A}from"./ParcelsList.styled-GDuguUBc.js";const F=h.ul`
   display: flex;
   justify-content: center;
 `,j=h.label`
