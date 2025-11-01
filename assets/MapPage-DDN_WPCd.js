f7ed3f65c58b47462c1c8155da651a91bfbec320
SlipuyKamiN <SlipuyKamiN@users.noreply.github.com>
2025-11-01 18:18:07 +0000

    deploy: bbb7575e4ac275a93d9da6ec4a6822734375c49d

--- assets/MapPage-DDN_WPCd.js
+++
@@ -1,1 +0,0 @@
-import{j as e,C as a}from"./index-GKb7kEcP.js";const s=()=>e.jsx(a,{children:e.jsx("section",{children:"MapPage"})});export{s as default};
--- assets/SettingsPage-CeksoNKV.js
+++
@@ -1,1 +0,0 @@
-import{j as t,C as e}from"./index-GKb7kEcP.js";const n=()=>t.jsx(e,{children:"SettingsPage"});export{n as default};
