d7f961ab8c99f14362d108baf8853fd2253fce70
SlipuyKamiN <SlipuyKamiN@users.noreply.github.com>
2025-11-07 17:19:04 +0000

    deploy: 4f699ba27b6907dff2f6c215202277bf155274ae

---
+++ assets/MapPage-B4-NhD4E.js
@@ -0,0 +1,1 @@
+import{j as e,C as a}from"./index-BZyQE7c2.js";const s=()=>e.jsx(a,{children:e.jsx("section",{children:"MapPage"})});export{s as default};
--- assets/SettingsPage-DYUNw_2M.js
+++
@@ -1,1 +0,0 @@
-import{j as t,C as e}from"./index-oRai7h1w.js";const n=()=>t.jsx(e,{children:"SettingsPage"});export{n as default};
