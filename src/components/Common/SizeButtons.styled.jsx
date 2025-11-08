536e1c1cd2c77ded361321053bbab941d1eac909
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-11-08 20:07:57 +0100

    fix: buttons refactoring

---
+++ src/components/Common/SizeButtons.styled.jsx
@@ -0,0 +1,119 @@
+import styled from "@emotion/styled";
+import { transition } from "~/styles/common/vars";
+
+export const SizeButtonsList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  margin-bottom: 20px;
+`;
+
+export const SizeLabel = styled.label`
+  display: block;
+  margin-bottom: 10px;
+`;
+
+export const SizeButton = styled.label`
+  position: relative;
+
+  display: block;
+  height: 60px;
+  width: 60px;
+
+  border-radius: 14px;
+  background-color: ${({ theme }) => theme.colors.hi200};
+
+  overflow: hidden;
+
+  cursor: pointer;
+
+  transition: all ${transition.duration};
+
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.mid100};
+  }
+
+  &:hover > span,
+  &:focus > span,
+  &:hover > svg,
+  &:focus > svg {
+    color: ${({ theme }) => theme.colors.lo200};
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 80px;
+    width: 80px;
+  }
+`;
+
+export const SizeInput = styled.input`
+  visibility: hidden;
+  width: 100%;
+  height: 100%;
+
+  &:checked + span {
+    color: ${({ theme }) => theme.colors.hi200};
+    background-color: ${({ theme }) => theme.colors.lo100};
+
+    & + span {
+      color: ${({ theme }) => theme.colors.hi200};
+    }
+
+    & svg {
+      opacity: 0.5;
+    }
+  }
+`;
+
+export const SizeText = styled.span`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  padding-left: 4px;
+
+  text-align: left;
+  font-size: 50px;
+  line-height: 1;
+  font-weight: 800;
+  transition: all ${transition.duration};
+  color: ${({ theme }) => `${theme.colors.lo200}60`};
+
+  & svg {
+    position: absolute;
+    top: 25px;
+    left: 25px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 60px;
+    padding-left: 6px;
+
+    & svg {
+      width: 50px;
+      height: 50px;
+
+      top: 30px;
+      left: 30px;
+    }
+  }
+`;
+
+export const SizeDescription = styled.span`
+  position: absolute;
+  bottom: 5px;
+  left: 6px;
+  font-size: 10px;
+
+  transition: all ${transition.duration};
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    bottom: 8px;
+    left: 8px;
+  }
+`;
---
+++ src/components/Home/Calculator.jsx
@@ -0,0 +1,18 @@
+import { Container, Section } from "../SharedLayout/SharedLayout.styled";
+import { SectionTitle } from "./Steps.styled";
+
+export const Calculator = () => {
+  return (
+    <Section>
+      <Container>
+        <SectionTitle>How much it costs</SectionTitle>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Container>
+    </Section>
+  );
+};
