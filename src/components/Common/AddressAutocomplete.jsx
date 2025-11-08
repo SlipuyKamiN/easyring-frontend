4bc34d197e9965089ba1ab3025f8e4aad66a0b03
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-11-08 20:25:46 +0100

    refactoring: address input

---
+++ src/components/Common/AddressAutocomplete.jsx
@@ -0,0 +1,37 @@
+import { Controller } from "react-hook-form";
+import { GeoAddressWrapper } from "./Form.styled";
+import {
+  GeoapifyContext,
+  GeoapifyGeocoderAutocomplete,
+} from "@geoapify/react-geocoder-autocomplete";
+import { useTranslation } from "react-i18next";
+
+export const AddressAutocomplete = ({ control, setValue, name }) => {
+  const { t } = useTranslation();
+
+  return (
+    <GeoAddressWrapper>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <GeoapifyContext apiKey="de6774ac4979423286c131f56e59ff31">
+            <GeoapifyGeocoderAutocomplete
+              placeholder={t("form.address")}
+              limit={5}
+              filterByCircle={{
+                lat: 52.52,
+                lon: 13.405,
+                radiusMeters: 30000,
+              }}
+              placeSelect={(value) => {
+                setValue(name, value);
+              }}
+              value={field.value?.properties?.formatted || ""}
+            />
+          </GeoapifyContext>
+        )}
+      />
+    </GeoAddressWrapper>
+  );
+};
--- src/components/Home/Calculator.jsx
+++ src/components/Home/Calculator.jsx
@@ -1,13 +1,24 @@
+import { useForm } from "react-hook-form";
+import { SizeButtons } from "../Common/SizeButtons";
 import { Container, Section } from "../SharedLayout/SharedLayout.styled";
 import { SectionTitle } from "./Steps.styled";
 
 export const Calculator = () => {
+  const { register } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      size: "S",
+    },
+  });
+
   return (
     <Section>
       <Container>
         <SectionTitle>How much it costs</SectionTitle>
         <ul>
-          <li></li>
+          <li>
+            <SizeButtons register={register} />
+          </li>
           <li></li>
           <li></li>
           <li></li>
